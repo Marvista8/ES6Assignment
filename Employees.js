cb32837a39e24ad6911d2aff7673e534ed4e506e
Margaret Farmer <86688818+Marvista8@users.noreply.github.com>
2022-03-13 22:30:32 -0400

    I am still having trouble  with the destructuring keeps running errors.

--- Employees.js
+++ Employees.js
@@ -8,9 +8,9 @@ class Employee {
     }
     getBook () {
        this.author = author;
-       this.title = title,
-       this.genre = genre,
-       this.year = year,
+       this.title = title;
+       this.genre = genre;
+       this.year = year;
        this.isbn = isbn;
        let book = {author} + {title} + {genre} + {year} + {isbn};
        return book;
@@ -29,7 +29,7 @@ class Employee {
 
 
 //console.log(Employee);
-const storeEmployee1 = new Employee('John', 'Doe', '01206856', '01001').addBook;
+const storeEmployee1 = new Employee('John', 'Doe', '01206856', '01001');
 const storeEmployee2= new Employee('Jane', 'Doe', '01216856', '01001');
 console.log(storeEmployee1);
 console.log(storeEmployee2);
@@ -48,11 +48,31 @@ console.log(storeManager);
 
 
 const countries = ['Finland', 'Estonia', 'Sweden', 'Denmark', 'Norway'];
-const [Finland, Estonia, Sweden, Denmark, Norway] = countries;
-const Regions = countries.map((countries) => {return 'fin', 'est', 'sw', 'den', 'nor'});    //Something wrong: Only returning new array with nor (Debugging)
-console.log(countries);
-console.log(Regions);
+const [Finland, Estonia, Sweden, Denmark, Norway] = countries; 
+const Regions = countries.map((fin) => {return fin});
+console.log(Regions)
+//Something wrong: Only returning new array with nor (Debugging)
+// console.log(countries);
+
 
 const rectangle = {
     width: 20,
+    height: 10,
+    area: 200, 
+    perimeter: 60,
 }
+
+myRectangle(rectangle);
+
+function myRectangle({width, height, area, perimeter}) {
+    const message = 'The rectangle is ' + width + ' centimeters wide and the height is ' + height + ' centimeters tall, with an area of ' + area + ' centimeters, and a perimeter of ' + perimeter + ' centimeters.';
+    return message;
+}
+
+// const evens = [0, 2, 4, 6, 8, 10];
+// const [0, 2, 4, 6, 8, 10] = evens;
+//constant typescript errors stating it's not a destructuring pattern
+
+// const odds = [1, 3, 5, 7, 9, 11];
+// const [1, 3, 5, 7, 9, 11] = odds;
+//don't know what I am doing wrong won't let me desctructure properly without 
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # ES6Assignment
 ### About the Assignment.
 
+IIn this project, I created some employee and manager classes, respectively. The primary class employee has permissions
+that are inherited by the manager class. The manager differs in having higher privileges as opposed to regular employees.
+The function to delete books, reset passwords, and create profiles for the staff is granted to the manager. The files created for the project are in the index.html file and Employees.js.
