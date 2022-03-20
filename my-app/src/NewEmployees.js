77c33b257a47574bf0481e1ae4a7c6a70aa455ae
Margaret Farmer <86688818+Marvista8@users.noreply.github.com>
2022-03-20 14:51:37 -0400

    This is just an update.

--- my-app/src/NewEmployees.js
+++ my-app/src/NewEmployees.js
@@ -1,5 +1,11 @@
-const userPermissions = {add: 'true', edit: 'true', view: 'true', delete: 'true'};
-console.log(userPermissions);
+const setPermissions = {
+    add: 'true',
+    edit: 'true',
+    view: 'true',
+    delete: 'true',
+};
+
+console.log('setPermissions: ', setPermissions);
 
 class Employee {
     constructor(fullName, idNumber, storeNumber, permissions) {
@@ -7,7 +13,7 @@ class Employee {
         this.idNumber = idNumber;
         this.storeNumber = storeNumber;
         this.permissions = permissions;
-        this.employmentType = Employee;
+        this.employeeType = 'Employee';
     }
 }
 
@@ -15,17 +21,44 @@ class Manager extends Employee {
     constructor(fullName, idNumber, storeNumber, permissions, employees) {
         super(fullName, idNumber, storeNumber, permissions)
         this.employees = employees;
-        this.employmentType = Manager;
+        this.employeeType = 'Manager';
     }
-    setUserPremissions(employee, setUserPermissions) {
-        let findEmployee = false;
-        console.log(findEmployee);
-
-        for(let i = 0; i < this.employees.length; i++) {
-        if (employee.idNumber === this.employees[i].idNumber)
-        console.log(employee.idNumber === this.employees[i].idNumber)
+    updatePermissions(employee, updatePermissionSet) {
+        let foundEmployee = false;
+        console.log('foundEmployee: ', foundEmployee);
+        for (let i = 0; i < this.employees[i].length; i++) {
+            if (employee.idNumber === this.employees[i].idNumber) {
+                employee.permissions = updatePermissionSet;
+                foundEmployee = true;
+                break;
+            }
         }
+
+        return employee;
     }
 }
+
+class Book {
+    constructor(title, author, genre, isbn, year) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.isbn = isbn;
+        this.year = year;
+    }
+}
+
+const staffMember1 = new Employee('Carlos Delgado', 102345, 100, null);
+const staffMember2 = new Employee('Holly Withers', 103586, 100, null);
+const staffMember3 = new Employee('Jonathan Miller', 104685, 100, null);
+
+console.log('Employee: ', Employee);
+console.log('staffMember1: ', staffMember1);
+console.log('staffMember2: ', staffMember2);
+console.log('staffMember3: ', staffMember3);
+
+const staffManager = new Manager('Michael Howard', 165894, 100, null);
+
+console.log('staffManager: ', staffManager);
 
 export default Employee;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import NewEmployees from './NewEmployees';
+import App from './App';
+import Employee from './NewEmployees';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<NewEmployees />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
