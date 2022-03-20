e096b12ec630e7884ac43178be35dc7a26dbfcb6
Margaret Farmer <86688818+Marvista8@users.noreply.github.com>
2022-03-20 15:20:48 -0400

    more changes for the same file.

--- my-app/src/NewEmployees.js
+++ my-app/src/NewEmployees.js
@@ -33,7 +33,6 @@ class Manager extends Employee {
                 break;
             }
         }
-
         return employee;
     }
 }
@@ -48,17 +47,25 @@ class Book {
     }
 }
 
-const staffMember1 = new Employee('Carlos Delgado', 102345, 100, null);
-const staffMember2 = new Employee('Holly Withers', 103586, 100, null);
-const staffMember3 = new Employee('Jonathan Miller', 104685, 100, null);
-
 console.log('Employee: ', Employee);
-console.log('staffMember1: ', staffMember1);
-console.log('staffMember2: ', staffMember2);
-console.log('staffMember3: ', staffMember3);
 
 const staffManager = new Manager('Michael Howard', 165894, 100, null);
 
 console.log('staffManager: ', staffManager);
+
+let staffMembers = () => {
+    let staffMember1 = new Employee('Carlos Delgado', 102345, 100, {add: 'true', edit: 'true', view: 'true', delete: 'false'});
+    let staffMember2 = new Employee('Holly Withers', 103568, 100, {add: 'true', edit: 'true', view: 'true', delete: 'false'});
+    let staffMember3 = new Employee('Jonathan Miller', 104685, 100, {add: 'true', edit: 'true', view: 'true', delete: 'false'});
+    console.log('staffMember1: ', staffMember1, 'staffMember2: ', staffMember2, 'staffMember3: ', staffMember3);
+}
+
+let staffManagers = () => {
+    let staffManager = new Manager('Michael Howard', 198753, 100, {add: 'true', edit: 'true', view: 'true', delete: 'true'});
+    console.log('staffManager: ', staffManager);
+}
+
+staffMembers();
+staffManagers();
 
 export default Employee;
